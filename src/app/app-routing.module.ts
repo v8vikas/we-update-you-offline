cf10da18330b70a9f4aa5929b0099922bcec0481
Vid <v8.vikas@gmail.com>
2018-10-16 23:53:59 +0530

    - init the angular application for the WPA (offline mode).

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { SplashComponent } from './layout/auth/splash/splash.component';
+import { LoginComponent } from './layout/auth/login/login.component';
+import { StartComponent } from './layout/feed/start/start.component';
+import { SettingsComponent } from './layout/settings/settings.component';
+import { NotificationComponent } from './layout/settings/notification/notification.component';
+
+
+export const routes: Routes = [
+  { path: '', component: SplashComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'feed/start', component: StartComponent, pathMatch: 'full' },
+  { path: 'settings', component: SettingsComponent, pathMatch: 'full' },
+  { path: 'settings/notification', component: NotificationComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { LayoutModule } from './layout/layout.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UICarouselModule } from 'ui-carousel';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    LayoutModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    UICarouselModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class AppModule { }
