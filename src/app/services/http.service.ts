0be1252b44b72f35eca7c9aa78190b5227de5e7b
Vid <v8.vikas@gmail.com>
2018-10-28 14:07:54 +0530

    - Updated the register page design

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http'
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class HttpService {
+  // application/x-www-form-urlencoded
   baseUrl: string = environment.baseUrl;
   constructor(private http: HttpClient) { }
+  getHeaders(): HttpHeaderResponse{
+    var headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers.append('Access-Control-Allow-Origin', '*');
+    return 
+  }
 
   get(endpoint: string): Observable<any> {
     return this.http.get(`${this.baseUrl}?${endpoint}`);
   }
 
-  post(endpoint: string, data: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}?${endpoint}`, data);
+  post(endpoint: string, headers: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}?${endpoint}`, {}, headers);
   }
 
-  put(endpoint: string, data: any): Observable<any> {
-    return this.http.get(`${this.baseUrl}?${endpoint}`, data);
+  put(endpoint: string, headers: any): Observable<any> {
+    return this.http.get(`${this.baseUrl}?${endpoint}`, headers);
   }
 
   delete(endpoint: string): Observable<any> {
